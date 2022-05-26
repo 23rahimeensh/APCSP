582b326d1e982b35d3b9d063addcaacfa43553d6
23rahimeensh <106180997+23rahimeensh@users.noreply.github.com>
2022-05-26 14:05:22 -0700

    Add files via upload

---
+++ new.js
@@ -0,0 +1,14 @@
+<script>
+        function show() {
+ 
+            /* Access image by id and change
+            the display property to block*/
+            document.getElementById('image')
+                    .style.display = "block";
+ 
+            document.getElementById('btnID')
+                    .style.display = "none";
+        }
+    </script>
+</body>
+</html>
